fd727d3cc0419c7590700ebd7222191543337622
Karlie-crypto <karlieemoyo@gmail.com>
2022-12-13 17:26:55 -0800

    monty

---
+++ stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * _stack - function that sets the format of the data to a stack (LIFO).
+ * @stack: double pointer to the head of stack.
+ * @line_number: script line number
+ *
+ * Return: No return.
+ */
+void _stack(stack_t **stack, unsigned int line_number)
+{
+	(void)(stack);
+	(void)(line_number);
+	var.queue = QUEUE;
+}
